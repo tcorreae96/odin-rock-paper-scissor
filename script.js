5bf279d5775c0d0a823f0582905c8460c32beea8
Tomas Correa <79479585+tcorreae96@users.noreply.github.com>
2024-07-23 16:56:02 -0400

    work in progress

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 let humanScore = 0;
 let computerScore = 0;
+let roundCount = 0;
+let playing = true;
+
+const humanChoices = document.querySelector("#choices");
+const startRoundBtn = document.querySelector("#startRound");
+const humanScoreText = document.querySelector("#humanScoreText");
+const computerScoreText = document.querySelector("#computerScoreText");
+const text = document.querySelector("#text");
+const roundText = document.querySelector("#roundText");
+let humanChoice;
+
+humanChoices.addEventListener("click", (e) => {
+	humanChoice = e.target.id;
+	if (pl)
+	playRound(humanChoice, getComputerChoice());
+});
+
+startRoundBtn.addEventListener("click", () => playGame());
+
+function isPlaying () {
+	if (playing === true) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
 function getComputerChoice () {
 	number = Math.floor(Math.random() * 9 + 1);
@@ -13,51 +39,65 @@ function getComputerChoice () {
 }
 
 function getHumanChoice () {
-  return prompt("Rock, Paper or Scissor?").toLowerCase();
+  return humanChoice;
+}
+
+function playGame () {
+	while (isPlaying()) {
+		if (humanScore === 5 || computerScore === 5) {
+			return playing = false;
+		}
+		playRound(humanChoice, getComputerChoice());
+	}
 }
 
 function playRound (humanChoice, computerChoice) {
+	roundCount++;
+	roundText.innerText = roundCount;
   if (humanChoice === "rock") {
     if (computerChoice === "paper") {
       computerScore++;
-      return `You lose ${computerChoice} beats ${humanChoice}`;
+			text.innerText = `You lose ${computerChoice} beats ${humanChoice}`;
+			computerScoreText.innerText = computerScore;
+      // return `You lose ${computerChoice} beats ${humanChoice}`;
     } else if (computerChoice === "scissor") {
       humanScore++;
-      return `You win ${humanChoice} beats ${computerChoice}`;
+			text.innerText = `You win ${humanChoice} beats ${computerChoice}`;
+			humanScoreText.innerText = humanScore;
+      // return `You win ${humanChoice} beats ${computerChoice}`;
     } else {
-      return "Its a tie";
+			text.innerText = "Its a tie";
+      // return "Its a tie";
     }
   } else if (humanChoice === "paper") {
     if (computerChoice === "scissor") {
       computerScore++;
-      return `You lose ${computerChoice} beats ${humanChoice}`;
+			text.innerText = `You lose ${computerChoice} beats ${humanChoice}`;
+			computerScoreText.innerText = computerScore;
+      // return `You lose ${computerChoice} beats ${humanChoice}`;
     } else if (computerChoice === "rock") {
       humanScore++;
-      return `You win ${humanChoice} beats ${computerChoice}`;
+			text.innerText = `You win ${humanChoice} beats ${computerChoice}`;
+			humanScoreText.innerText = humanScore;
+      // return `You win ${humanChoice} beats ${computerChoice}`;
     } else {
-      return "Its a tie";
+			text.innerText = "Its a tie";
+      // return "Its a tie";
     }
   } else {
     if (computerChoice === "rock") {
       computerScore++;
-      return `You lose ${computerChoice} beats ${humanChoice}`;
+			text.innerText = `You lose ${computerChoice} beats ${humanChoice}`;
+			computerScoreText.innerText = computerScore;
+      // return `You lose ${computerChoice} beats ${humanChoice}`;
     } else if (computerChoice === "paper") {
       humanScore++;
-      return `You win ${humanChoice} beats ${computerChoice}`;
+			text.innerText = `You win ${humanChoice} beats ${computerChoice}`;
+			humanScoreText.innerText = humanScore;
+      // return `You win ${humanChoice} beats ${computerChoice}`;
     } else {
-      return "Its a tie";
+			text.innerText = "Its a tie";
+      // return "Its a tie";
     }
   }
 }
-
-
-function playGame () {
-  for (let i = 0; i < 5 ; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    console.log(playRound(humanSelection, computerSelection));
-    console.log(`Human Score: ${humanScore}\nComputer Score: ${computerScore}`);
-  }
-}
-
-playGame();
